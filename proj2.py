81bf00cafc0051f40a3af6a1f70d55b7ce741eac
Matheus M. Jeronimo <matheus.martins.jeronimo@gmail.com>
2020-07-04 11:28:09 -0300

    Create proj2.py
    Cria esqueleto do projeto

---
+++ proj2.py
@@ -0,0 +1,15 @@
+# 11-20 07-07-2020
+#
+# Projeto 2 - Mad Libs Generator
+#
+# Objetivo: computador sorteia um número e o usuário deve adivinha-lo
+#
+# Inspired by Summer Son’s Mad Libs project with Javascript.
+#
+# Nota: o que é uma 'mad lib': http://www.redkid.net/madlibs/
+
+class MadLib:
+    pass
+
+class Formulario:
+    pass
